bc0a2afb0a24111f1ede6340a9c0394aaf3a99de
Lucas Apparecido de Oliveira <108204218+LucasApparecido@users.noreply.github.com>
2023-10-11 10:09:25 -0300

    test

--- src/main/java/ueg/Main.java
+++ src/main/java/ueg/Main.java
@@ -1,10 +1,9 @@
 package ueg;
 
-import ueg.front.Screen;
-
+import static ueg.front.Screen.getInstance;
 public class Main {
     public static void main(String[] args) {
         //inicia o programa
-        Screen screen = new Screen();
+        getInstance();
     }
 }
--- src/main/java/ueg/front/Screen.java
+++ src/main/java/ueg/front/Screen.java
@@ -1,4 +1,49 @@
 package ueg.front;
 
-public class Screen {
+import javax.swing.*;
+import java.awt.*;
+
+public class Screen extends JFrame {
+    private static Screen instance;
+
+    public static final int width = 800;
+    public static final int height = 600;
+
+    public JTextField write;
+
+    private Screen (){
+        setTitle("Chat - User");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(Screen.width, Screen.height);
+        setResizable(false);
+        setLocation(100, 100);
+        getContentPane().setLayout(new BorderLayout());
+        setIconImage(new ImageIcon(getClass().getResource("/icon.png")).getImage());
+
+        //buildLayout();
+
+        setVisible(true);
+    }
+
+    public static Screen getInstance() {
+        if (instance == null) {
+            instance = new Screen();
+        }
+        return instance;
+    }
+
+    private void buildLayout(){
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color (1,32,15));
+
+        write = new JTextField();
+        write.setFont(new Font("Times New Roman", Font.BOLD, 24));
+        write.setHorizontalAlignment(JTextField.RIGHT);
+        //setWriteSize(display, 300, 50);
+        write.setBackground(new Color(1,32,15));
+        write.setEnabled(false);
+        mainPanel.add(write, BorderLayout.SOUTH);
+    }
+
 }
